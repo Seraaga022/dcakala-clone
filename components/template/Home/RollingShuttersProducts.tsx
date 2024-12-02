6282e317fa59afd59811c51331715682e105ceb6
mhmd ali <serever100@gmail.com>
2024-12-02 11:30:41 +0330

    rolling shutter banners fixed responsiveness

--- components/template/Home/RollingShuttersProducts.tsx
+++ components/template/Home/RollingShuttersProducts.tsx
@@ -1,4 +1,6 @@
 import { Box, Container } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import Image from "next/image";
 import React from "react";
 
 const RollingShuttersProducts = () => {
@@ -12,57 +14,29 @@ const RollingShuttersProducts = () => {
           },
         }}
       >
-        <Box
-          display="flex"
-          gap={{ xs: "8px", xl: "15px" }}
-          justifyContent="end"
-        >
-          {/* left */}
-          <Box mr="auto">
-            <Box component="picture">
-              <source
-                srcSet="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_original.webp , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_2x.webp 2x , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_3x.webp 3x"
-                type="image/webp"
+        <Box height={{ xs: "60px", sm: "100px", md: "180px", lg: "230px" }}>
+          <Grid height="100%" container spacing={{ xs: 1, lg: 2 }}>
+            {/* left */}
+            <Grid size={6} position="relative">
+              <Image
+                src="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_3x.jpg"
+                alt="roller-shutters-banner1"
+                objectFit="cover"
+                fill
+                style={{ borderRadius: "7px" }}
               />
-              <source
-                srcSet="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_original.jpg , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_2x.jpg 2x , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_3x.jpg 3x"
-                type="image/jpg"
+            </Grid>
+            {/* right */}
+            <Grid size={6} position="relative">
+              <Image
+                src="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_3x.jpg"
+                alt="roller-shutters-banner1"
+                objectFit="cover"
+                fill
+                style={{ borderRadius: "7px" }}
               />
-              <Box
-                component="img"
-                srcSet="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_original.jpg , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_2x.jpg 2x , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_3x.jpg 3x"
-                src="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-left_original.jpg"
-                alt="موتور کرکره برقی طرح دسکتاپ سمت چپ"
-                title="موتور کرکره برقی طرح دسکتاپ سمت چپ"
-                width={{ xs: "44vw", sm: "45vw", xl: 655 }}
-                height={{ xs: "auto", xl: 235 }}
-                style={{ borderRadius: "0.5rem" }}
-              />
-            </Box>
-          </Box>
-          {/* right */}
-          <Box>
-            <Box component="picture">
-              <source
-                srcSet="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_original.webp , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_2x.webp 2x , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_3x.webp 3x"
-                type="image/webp"
-              />
-              <source
-                srcSet="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_original.jpg , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_2x.jpg 2x , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_3x.jpg 3x"
-                type="image/jpg"
-              />
-              <Box
-                component="img"
-                srcSet="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_original.jpg , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_2x.jpg 2x , https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_3x.jpg 3x"
-                src="https://dashboard.dcakala.com/public/images/home/simple-banner/2024/09/roller-shutter-motor-desktop-right_original.jpg"
-                alt="موتور کرکره برقی طرح دسکتاپ سمت راست"
-                title="موتور کرکره برقی طرح دسکتاپ سمت راست"
-                width={{ xs: "44vw", sm: "45vw", xl: 655 }}
-                height={{ xs: "auto", xl: 235 }}
-                style={{ borderRadius: "0.5rem" }}
-              />
-            </Box>
-          </Box>
+            </Grid>
+          </Grid>
         </Box>
       </Container>
     </Box>
